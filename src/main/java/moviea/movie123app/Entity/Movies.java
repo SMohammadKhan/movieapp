243479ef5d51b0c572a8ae936f4228cc439456bd
SMohammadKhan <iammohammad65@gmail.com>
2024-09-30 22:51:50 +0530

    new

--- src/main/java/moviea/movie123app/Entity/Movies.java
+++ src/main/java/moviea/movie123app/Entity/Movies.java
@@ -1,5 +1,7 @@
 package moviea.movie123app.Entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,47 +9,82 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Movies")
+@Table(name = "movies")
 public class Movies {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String mname;
+
+    private String title;
     private String genre;
-    private boolean ispopular;
-    public Long getId() {
-        return id;
+    private boolean isPopular;
+    private String poster;
+     private LocalDate releaseDate; 
+     
+    
+
+    public void setPopular(boolean isPopular) {
+        this.isPopular = isPopular;
     }
-    public void setId(Long id) {
-        this.id = id;
+
+    public String getPoster() {
+        return poster;
     }
-    public String getMname() {
-        return mname;
-    }
-    public void setMname(String mname) {
-        this.mname = mname;
-    }
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    public boolean isIspopular() {
-        return ispopular;
-    }
-    public void setIspopular(boolean ispopular) {
-        this.ispopular = ispopular;
-    }
-    public Movies(Long id, String mname, String genre, boolean ispopular) {
-        this.id = id;
-        this.mname = mname;
-        this.genre = genre;
-        this.ispopular = ispopular;
-    }
-    public Movies() {
+
+    public void setPoster(String poster) {
+        this.poster = poster;
     }
 
     
+    public Movies() {
+    }
+
+    public Movies(String title, String genre, boolean isPopular,String poster,LocalDate date) {
+        this.title = title;
+        this.genre = genre;
+        this.isPopular = isPopular;
+        this.poster=poster;
+        this.releaseDate=date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean getIsPopular() {
+        return isPopular;
+    }
+
+    public void setIsPopular(boolean isPopular) {
+        this.isPopular = isPopular;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
